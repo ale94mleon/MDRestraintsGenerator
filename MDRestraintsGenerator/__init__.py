ee05859908d2558fa3fe41d261568a43d9dd182d
Irfan Alibay <IAlibay@users.noreply.github.com>
2025-10-12 00:21:40 +0100

    Bump up CI, etc... (#51)
    * Bump up CI, etc...
    
    * pytest auto and codecov token
    
    * update everything
    
    * LGPLv3+ why are you so annoying?
    
    * drop python 3.10, we're SPEC0
    
    * try to fix versioning

--- MDRestraintsGenerator/__init__.py
+++ MDRestraintsGenerator/__init__.py
@@ -2,13 +2,6 @@
 MDRestraintsGenerator
 A framework for generating restraints for MD simulations
 """
-
-# Add imports here
-# from .restraints import *
-
-# Handle versioneer
-from ._version import get_versions
-versions = get_versions()
-__version__ = versions['version']
-__git_revision__ = versions['full-revisionid']
-del get_versions, versions
+# Handle version
+from importlib.metadata import version
+__version__ = version("MDRestraintsGenerator")
